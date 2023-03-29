11406cf8e4245a2d6f5bda5c903c8c847b17c2ef
Maksim Iogan <54811685+maksiam@users.noreply.github.com>
2023-03-29 16:56:52 +0300

    Update script.py
    Changed paths

--- script.py
+++ script.py
@@ -3,11 +3,11 @@ import pickle
 import implicit
 from operator import itemgetter
 
-with open("D:\\Загрузки\\model (1).pkl", "rb") as f:
+with open("src\\model.pkl", "rb") as f:
     model = pickle.load(f)
-with open("D:\\Загрузки\\title_encode.pkl", "rb") as f:
+with open("src\\title_encode.pkl", "rb") as f:
     title_encode = pickle.load(f)
-with open("D:\\Загрузки\\title_decode.pkl", "rb") as f:
+with open("src\\title_decode.pkl", "rb") as f:
     title_decode = pickle.load(f)
 artist = input("Исполнитель: ")
 if title_encode.get(artist) == None:
